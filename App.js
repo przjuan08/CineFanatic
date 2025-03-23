9a14f2a839705b7ebbd415573268f631f8fff194
marlon-027ort <oc232936@alumno.udb.edu.sv>
2025-03-22 18:06:01 -0600

    Terminando App.Js

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import SplashScreen from "./components/screens/SplashScreen"
+import HomeScreen from "./components/screens/HomeScreen"
+import DetailsScreen from "./components/screens/DetailsScreen"
+import ProfileScreen from "./components/screens/ProfileScreen"
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Splash"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#032541",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      >
+        <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: "CineFanatic" }} />
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          options={({ route }) => ({ title: route.params?.title || "Movie Details" })}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} options={{ title: "Mi perfil" }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
